65625cc98df623d5cf2452d65fb670d54b0bceca
Prateek <psachan@cs.iitr.ac.in>
2019-06-03 21:41:06 +0530

    omit empty json values

--- api/jsonModels.go
+++ api/jsonModels.go
@@ -8,18 +8,24 @@ type MovieAlias Movie
 //MovieJSON model for json
 type MovieJSON struct {
 	MovieAlias
-	Actors []string `json:"actors"`
+	Actors    []string `json:"actors,omitempty"`
+	PosterURI string   `json:"poster,omitempty"`
 }
 
 //MarshalJSON implementing marshaler interface
 func (m Movie) MarshalJSON() ([]byte, error) {
 	var actName []string
+	var posterURI string
 	for _, actor := range m.Actors {
 		actName = append(actName, actor.Name)
+	}
+	if m.PosterURI != "" {
+		posterURI = "media/posters/" + m.PosterURI
 	}
 	mj := MovieJSON{
 		MovieAlias: MovieAlias(m),
 		Actors:     actName,
+		PosterURI:  posterURI,
 	}
 	return json.Marshal(mj)
 }
